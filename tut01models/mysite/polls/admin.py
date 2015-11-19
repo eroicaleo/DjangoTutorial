220b8e8d22741f14034f358a1b4b73a166aed1ff
Yang Ge <eroicaleo@gmail.com>
2015-11-18 18:04:08 -0800

    updates tutorial 2 Customize the admin form

--- tut01models/mysite/polls/admin.py
+++ tut01models/mysite/polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Melon',     {'fields': ['question_text']}),
+        ('Date info', {'fields': ['pub_date'], 'classes': ['collapse']})
+    ]
+
+admin.site.register(Question, QuestionAdmin)
